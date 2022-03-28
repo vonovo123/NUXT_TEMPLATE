45880b7e4f58d6de8db90d0a1173410d7d2a8c83
dynamic-henry-kwon <jukdo67@gmail.com>
2022-03-28 15:36:36 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div> {{ data }}</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  asyncData({app}){
+
+    return {
+
+    }
+  },
+  data() {
+    return {
+      data: null
+    }
+  },
+  methods: {
+    async setData() {
+      this.data = await fetch();
+    }
+  },
+  components: {
+  },
+  created () {
+    this.setData();
+  },
+
+}
+
+async function fetch(){
+  const payLoad = {
+    title :'frozen',
+    type: 'movie',
+    year : '2020',
+    page: 1
+  }
+  console.log(process.client)
+  const url = process.client
+    ? '/api/test'
+    : `${process.env.CLIENT_URL}/api/test`
+  return await axios.post(url, payLoad)
+}
+</script>
